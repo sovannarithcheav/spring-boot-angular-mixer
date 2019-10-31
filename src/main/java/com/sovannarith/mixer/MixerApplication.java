fdbdaaa8ed8e00d8405961e40655555b05f505e2
Cheav Sovannarith <cheavsovannarith@gmail.com>
2019-10-31 14:08:16 +0700

    init project

---
+++ src/main/java/com/sovannarith/mixer/MixerApplication.java
@@ -0,0 +1,42 @@
+package com.sovannarith.mixer;
+
+import com.mixer.api.MixerAPI;
+import com.mixer.api.services.impl.ChannelsService;
+import com.sovannarith.mixer.model.User;
+import com.sovannarith.mixer.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class MixerApplication implements ApplicationRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MixerApplication.class, args);
+    }
+
+    private MixerAPI mixer = new MixerAPI("");
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        ChannelsService channelsService = new ChannelsService(mixer);
+        channelsService.findOneByToken("");
+    }
+
+    @Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@domain.com");
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
+    }
+}
---
+++ src/main/java/com/sovannarith/mixer/model/User.java
@@ -0,0 +1,28 @@
+package com.sovannarith.mixer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String email;
+
+}
